d20a6c15764e8753765c66d59291c607d6c13cf9
LincolnK <lincoln.kinley@siu.edu>
2017-03-02 09:18:02 -0600

    Update structures.h

--- structures.h
+++ structures.h
@@ -2,26 +2,30 @@
 define STRUCTURES_H_
 
 struct student {
-  char ID[16];
+  int ID;
   char password[32];
   char firstName[16];
   char lastName[16];
+  struct student *next;
 };
 
 struct faculty {
-  char ID[16];
+  int ID;
   char password[32];
   char firstName[16];
   char lastName[16];
+  struct faculty *next;
 };
 
 struct class {
   char title[32];
+  struct class *next;
 };
 
 struct vacancy {
   char position[32];
   char discription[128];
+  struct vacancy *next;
 };
 
 #endif // STRUCTURES_H_
