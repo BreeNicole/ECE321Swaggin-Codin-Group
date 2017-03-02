cce79b6b6a97cdb78a7227ec77ae3e2e932832a7
LincolnK <lincoln.kinley@siu.edu>
2017-03-02 10:26:52 -0600

    Update LincolnFunctions.c

--- LincolnFunctions.c
+++ LincolnFunctions.c
@@ -5,7 +5,7 @@ int run(int x)
   return x + 5;
 }
 
-void newVacancy(char posIn[32], charsidIn[128])
+void newVacancy(char posIn[32], char disIn[128])
 {
   struct vacancy *link = (struct vacancy*)malloc(sizeof(struct(vacancy));
   link->position = posIn;
