9e28b5b5bfefc305cd91ccdb384ac33b5d3898a1
LincolnK <lincoln.kinley@siu.edu>
2017-03-02 09:42:53 -0600

    Update LincolnFunctions.c

--- LincolnFunctions.c
+++ LincolnFunctions.c
@@ -4,3 +4,17 @@ int run(int x)
 {
   return x + 5;
 }
+
+void newVacancy()
+{
+  char posIn[32], disIn[128];
+  printf("Enter the position of the new vacancy (32 char limit)");
+  gets(posIn);
+  printf("Enter the position of the new vacancy (128 char limit)");
+  gets(disIn);
+  struct vacancy *link = (struct vacancy*)malloc(sizeof(struct(vacancy));
+  link->position = posIn;
+  link->discription = disIn;
+  link->next = *head;
+  head = link;
+}
