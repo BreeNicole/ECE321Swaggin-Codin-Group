e4a533ec5ed78f7a1f20ce7c14aa1e9206480b6b
LincolnK <lincoln.kinley@siu.edu>
2017-03-09 10:19:20 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -13,29 +13,29 @@ using namespace std;
 
 void main()
 {
-	cout << This is the best program, all others are bad << endl;
+	cout << "This is the best program, all others are bad" << endl;
 	int num = 3;
-	int z = run(num); // This will set z to be num + 5, since the run fuction adds five to whatever number it is passed
+	int z = add(num, 3); // This will set z to be num + 5, since the run fuction adds five to whatever number it is passed
 	char input;
 	int program = 0;
-	
-	while(program == 0)
+
+	while (program == 0)
 	{
 		printf("Login Function\n");
 		program = 1;
 	}
-	
+
 
 	while (program == 1)
 	{
 		printf("Enter your choice: ");
 		do
-			cin >> input;
+		cin >> input;
 		while (isspace(input));
 		switch (input) {
 
 		case '1':
-			printf("Run Function 1\n"); 
+			printf("Run Function 1\n");
 			break;
 
 		case '2':
@@ -73,14 +73,14 @@ void main()
 
 		case 'A':
 			printf("Run Function A\n"); //Post vacancies (Admin)
-				
-			string posIn, disIn;
-  			printf("Enter the position of the new vacancy");
- 			cin >> posIn;
-  			printf("Enter the discription of the new vacancy");
-  			cin >> disIn;
-			newVacancy(posIn, disIn);
-				
+
+			/*string posIn, disIn;
+			printf("Enter the position of the new vacancy");
+			cin >> posIn;
+			printf("Enter the discription of the new vacancy");
+			cin >> disIn;*/
+			//newVacancy(posIn, disIn);
+
 			break;
 
 		case 'B':
@@ -100,10 +100,10 @@ void main()
 
 void readfile() // This function will read the file and input the data in to the program. The .txt file should be saved in a particular order, depending on the type of structure it is
 {
-	
+
 }
 
 void savefile()
 {
-	
+
 }
