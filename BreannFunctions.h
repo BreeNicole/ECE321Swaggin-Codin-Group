f757c3edc020e8e0e7f692d0f399fc0c91e4a629
Breann Birch <breannbirch@yahoo.com>
2017-03-30 15:49:18 -0500

    Update BreannFunctions.h

--- BreannFunctions.h
+++ BreannFunctions.h
@@ -1,6 +1,14 @@
 #ifndef BREANNFUNCTIONS_H_
 #define BREANNFUNCTIONS_H_
 
+void ViewTimeTable();
 
+void ViewSelfAttend();
+
+void ViewAllAttend();
+
+void ViewSub();
+
+void ViewVac();
 
 #endif // BREANNFUNCTIONS_H_
