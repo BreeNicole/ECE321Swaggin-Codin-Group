31cbf08a226040c00be725e652f4616719276537
LincolnK <lincoln.kinley@siu.edu>
2017-04-03 17:54:44 -0500

    Update structures.h

--- structures.h
+++ structures.h
@@ -7,16 +7,53 @@ class user {
   string firstName;
   string lastName;
   char group; // S (student), F (faculty)
+public:
+  user(int a, string b, string c, string d, char e)
+  {
+    ID = a;
+    password = b;
+    firstName = c;
+    lastname = d;
+    group = e;
+  }
+  get_ID();
+  {
+    return ID;
+  }
+  get_firstName()
+  {
+    return firstName;
+  }
+  get_lastName()
+  {
+    return lastName;
+  }
+  get_group()
+  {
+    return group;
+  }
 };
 
 class course {
   string title;
   string attendance [30][2]; // ID will go in [x][1], (present / notpresent) will go in [x][2], this allots for 30 people in each class (we might need more)
+public:
+  course(string a, string b[30][2])
+  {
+   title = a;
+    attendance = b;
+  }
 };
 
 class vacancy {
   string position;
   string discription;
+public:
+  vacancy(string a, string b)
+  {
+    position = a;
+    discription = b;
+  }
 };
 
 
