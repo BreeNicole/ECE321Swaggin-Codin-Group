d1608a7e52719f9219dd35ed262ff6c3dec39f71
Breann Birch <breannbirch@yahoo.com>
2017-04-04 14:17:09 -0500

    Update BreannFunctions.cpp

--- BreannFunctions.cpp
+++ BreannFunctions.cpp
@@ -7,21 +7,21 @@ void ViewTimeTable() // Will allow the user to access the Time Table created by 
 
 void ViewSelfAttend() // Inside Function: choose a class then this function will allow the user to view their attendance
 {
-
+  // will call the function from course struct studAttendance
 }
 
 void ViewAllAttend() // Inside Function: choose a class then this function will allow  the user to view all attendance
 {
-  
+  // will call the function from course struct allAttendance
 }
 
 void ViewSub() // allows the user (student) to view their submissions into their classes
 {
-  
+  //will call the function from course struct studSub
 }
 
 void ViewVac() // allows the user to view the job vacanies posted by the faculty
 {
-  
+  // will call the function from vacancy struct viewVac
 }
 
