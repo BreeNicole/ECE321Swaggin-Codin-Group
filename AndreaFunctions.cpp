624a518f64662116080d788bc03120032f77675f
andipyle14 <andipyle14@gmail.com>
2017-04-10 11:54:42 -0500

    Update AndreaFunctions.cpp

--- AndreaFunctions.cpp
+++ AndreaFunctions.cpp
@@ -1,15 +1,5 @@
 #include "AndreaFunctions.h"
 
-void ViewVacancies()
-{
-  int n = sizeof(vacancy)/sizeof(vacancy[0]);
-  for (int i = 0; i < n; i++)
-  {
-    
-  }
-  
-};
-
 void StoreTimetable()
 {
   
