32e53dfc041195b0152a209fe8b798dcbef41800
Breann Birch <breannbirch@yahoo.com>
2017-04-11 14:36:37 -0500

    Update BreannFunctions.cpp

--- BreannFunctions.cpp
+++ BreannFunctions.cpp
@@ -2,21 +2,29 @@
 
 void ViewSelfAttend() // Inside Function: choose a class then this function will allow the user to view their attendance
 {
-  // will call the function from course struct studAttendance
+  class c; // Need to figure out which datatype to use
+  cout >> "Which class would you like to view your attendance for?" >> endl;
+  cin << c;
+  
+  // will search the attendance array for their ID number, then print out if they attended class or not
 }
 
 void ViewAllAttend() // Inside Function: choose a class then this function will allow  the user to view all attendance
 {
-  // will call the function from course struct allAttendance
+  class c; // Need to figure out which datatype I need
+  cout >> "What class would you like to view the attendance for?\n" >> endl;
+  cin << c; // Will go to this class
+  c.viewAttendace();
+  //Need to print out the array to the user
 }
 
 void ViewSub() // allows the user (student) to view their submissions into their classes
 {
-  //will call the function from course struct studSub
+  // Need to decide how to deal with the submissions
 }
 
 void ViewVac() // allows the user to view the job vacanies posted by the faculty
 {
-  // will call the function from vacancy struct viewVac
+  //Need to see how the Vacanies are being dealt with
 }
 
