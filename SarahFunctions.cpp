b917361694ac24e155159ec2faa0637e47c8cb9a
shandrock <s.handrock@siu.edu>
2017-04-13 13:50:39 -0500

    Update SarahFunctions.cpp

--- SarahFunctions.cpp
+++ SarahFunctions.cpp
@@ -1,1 +1,17 @@
 #include "SarahFunctions.h"
+#include "structures.h"
+#include <list>
+#include <iostream>
+#include <iterator>
+#include <string>
+
+int logIn(list<q>)
+{
+  string queryID;
+  string queryPass;
+  
+  for (list<q>::iterator it = q.begin(); it != q.end(); ++it)
+	{
+		if(it->ID == 
+	}
+}
