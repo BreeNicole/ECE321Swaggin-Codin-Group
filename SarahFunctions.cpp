a7277cbee3aac032c480f0911ed765305f637836
shandrock <s.handrock@siu.edu>
2017-04-20 11:06:47 -0500

    Update SarahFunctions.cpp

--- SarahFunctions.cpp
+++ SarahFunctions.cpp
@@ -9,9 +9,16 @@ int logIn(list<q>)
 {
   string queryID;
   string queryPass;
+  cout<<"Please enter your ID: ";
+  getline(cin, queryID);
+  cout<<"Please enter your password:  ";
+  getline(cin, queryPass);
   
   for (list<q>::iterator it = q.begin(); it != q.end(); ++it)
 	{
-		if(it->ID == 
+		if(it->ID ==queryID && strcmp(it->password, queryPass)==0)
+		{
+			return 1;
+		}
 	}
 }
