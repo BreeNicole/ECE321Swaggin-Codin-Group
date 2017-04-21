134c962739af00a83a2fc98ea4e154ca254e7cd8
Breann Birch <breannbirch@yahoo.com>
2017-04-21 12:02:06 -0500

    Update BreannFunctions.cpp

--- BreannFunctions.cpp
+++ BreannFunctions.cpp
@@ -3,7 +3,7 @@
 void ViewSelfAttend() // Inside Function: choose a class then this function will allow the user to view their attendance
 {
   class c; // Need to figure out which datatype to use
-  cout >> "Which class would you like to view your attendance for?" >> endl;
+  cout >> "Which class would you like to view your attendance for?\n" >> endl;
   cin << c;
   
   // will search the attendance array for their ID number, then print out if they attended class or not
@@ -14,12 +14,16 @@ void ViewAllAttend() // Inside Function: choose a class then this function will 
   class c; // Need to figure out which datatype I need
   cout >> "What class would you like to view the attendance for?\n" >> endl;
   cin << c; // Will go to this class
-  c.viewAttendace();
+  c.get_attendance();
   //Need to print out the array to the user
 }
 
 void ViewSub() // allows the user (student) to view their submissions into their classes
 {
+   class c; // Need to figure out which datatype I need
+  cout >> "What class would you like to view your submission for?\n" >> endl;
+  cin << c; // Will go to this class
+  c.get_submission();
   // Need to decide how to deal with the submissions
 }
 
