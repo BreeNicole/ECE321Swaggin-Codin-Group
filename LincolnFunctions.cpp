81fd4889523aab8dd445e38689c53399d6548615
LincolnK <lincoln.kinley@siu.edu>
2017-04-22 18:09:39 -0500

    Update LincolnFunctions.cpp
    Moved save and read files to my functions, and made a few access functions

--- LincolnFunctions.cpp
+++ LincolnFunctions.cpp
@@ -3,15 +3,47 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <list>
+#include <iterator>
 
-int run(int x)
+void newVacancy(list<vacancy>& vacList, string positionIn, string discriptionIn)
 {
-  return x + 5;
+	vacList.push_back(vacancy(positionIn,discriptionIn));
 }
 
-void newVacancy(string posIn, string disIn)
+void newUser(list<user>& userList, int ID, string password, string firstName, string lastName, char group)
 {
+	userList.push_back(user(ID, password, firstName, lastName, "", group));
+}
 
+void removeVacancy(list<vacancy>& vacList, string del)
+{
+	list<vacancy>::iterator it;
+	for (it = vacList.begin(); it != vacList.end(); ++it)
+	{
+		if (del == it->get_position())
+		{
+			vacList.erase(it);
+			cout << del << " has been removed" << endl;
+			return;
+		}
+	}
+	cout << "Position not found, no changes have been made." << endl;
+}
+
+void removeUser(list<user>& userList, int ID)
+{
+	list<user>::iterator it = userList.begin();
+	for (it = userList.begin(); it != userList.end(); ++it)
+	{
+		if (ID == it->get_ID())
+		{
+			userList.erase(it);
+			cout << ID << " has been deleted" << endl;
+			return;
+		}
+	}
+	cout << "ID not found, no changes have been made." << endl;
 }
 
 list<user> readUsers() // This function will read the file and input the data in to the program. The .txt file should be saved in a particular order, depending on the type of structure it is
@@ -25,99 +57,107 @@ list<user> readUsers() // This function will read the file and input the data in
 	size_t pos = 0;
 	string token;
 	infile.open("users.txt");
-	if(infile.is_open()) // This is going to be a problem if the file exists but it's empty (which shouldn't happen unless you manually edit the txt file), I'll look into a fix
+	if (infile.is_open()) // This is going to be a problem if the file exists but it's empty (which shouldn't happen unless you manually edit the txt file), I'll look into a fix
 	{
-		while( getline (infile,line))
+		while (getline(infile, line))
 		{
-			while(( pos = line.find("/")) != string::npos) // Nobody can have a / in their name, that will screw up the parsing. If it becomes a problem I will change the delimiter
+			while ((pos = line.find("/")) != string::npos) // Nobody can have a / in their name, that will screw up the parsing. If it becomes a problem I will change the delimiter
 			{
-				token = line.substr(0,pos);
+				token = line.substr(0, pos);
 				dataArray[i] = token;
-				line.erase(0,pos + 1);
+				line.erase(0, pos + 1);
 				++i;
 			}
+			i = 0;
 			int dataZero = atoi(dataArray[0].c_str());
 			char dataFive = line[0];
-			readUser.push_back(user(dataZero, dataArray[1], dataArray[2], dataArray[3], dataFive));
+			readUser.push_back(user(dataZero, dataArray[1], dataArray[2], dataArray[3], dataArray[4], dataFive));
 		}
 		infile.close();
 	}
 	else
 	{
-		cout << "File fialed to open" << endl;
-		readUser.push_back(user(1, "password", "admin", "", 'A'));
+		cout << "users.txt failed to open" << endl;
+		readUser.push_back(user(1, "password", "admin", "", "", 'A'));
 	}
 	return readUser;
 }
 
-list<course> readCourses() // This function will read the file and input the data in to the program. The .txt file should be saved in a particular order, depending on the type of structure it is
+list<vacancy> readVacs()
 {
-	list<course> readCourse;
-	list<course>::iterator p = readCourse.begin();
+	list<vacancy> readVac;
+	list<vacancy>::iterator p = readVac.begin();
 	string line;
 	ifstream infile;
 	int i = 0;
 	string dataArray[5];
 	size_t pos = 0;
 	string token;
-	infile.open("courses.txt");
-	if(infile.is_open()) // This is going to be a problem if the file exists but it's empty (which shouldn't happen unless you manually edit the txt file), I'll look into a fix
+	infile.open("vacancies.txt");
+	if (infile.is_open())
 	{
-		while( getline (infile,line))
+		while (getline(infile, line))
 		{
-			while(( pos = line.find("/")) != string::npos) // Nobody can have a / in their name, that will screw up the parsing. If it becomes a problem I will change the delimiter
+			while ((pos = line.find("/")) != string::npos)
 			{
-				token = line.substr(0,pos);
+				token = line.substr(0, pos);
 				dataArray[i] = token;
-				line.erase(0,pos + 1);
+				line.erase(0, pos + 1);
 				++i;
 			}
-			int dataZero = atoi(dataArray[0].c_str());
-			char dataFive = line[0];
-			readUser.push_back(user(dataZero, dataArray[1], dataArray[2], dataArray[3], dataFive));
+			dataArray[i] = line;
+			i = 0;
+			readVac.push_back(vacancy(dataArray[0], dataArray[1]));
 		}
 		infile.close();
 	}
 	else
 	{
-		cout << "File fialed to open" << endl;
-		readUser.push_back(user(1, "password", "admin", "", 'A'));
+		cout << "vacancies.txt failed to open" << endl;
+		readVac.push_back(vacancy("Temp", "Please remove this after you add a vacancy"));
 	}
-	return readCourse;
+	return readVac;
 }
 
-list<vacancy> readVacancies() // This function will read the file and input the data in to the program. The .txt file should be saved in a particular order, depending on the type of structure it is
+void saveUsers(list<user> saveUser)
 {
-	list<vacancy> readVacancy;
-	list<vacancy>::iterator p = readVacancy.begin();
-	string line;
-	ifstream infile;
-	int i = 0;
-	string dataArray[2];
-	size_t pos = 0;
-	string token;
-	infile.open("vacancies.txt");
-	if(infile.is_open()) // This is going to be a problem if the file exists but it's empty (which shouldn't happen unless you manually edit the txt file), I'll look into a fix
+	ofstream outfile;
+	outfile.open("users.txt");
+	if (outfile.is_open())
 	{
-		while( getline (infile,line))
+		for (list<user>::iterator it = saveUser.begin(); it != saveUser.end(); it++)
 		{
-			while(( pos = line.find("/")) != string::npos) // Nobody can have a / in their name, that will screw up the parsing. If it becomes a problem I will change the delimiter
-			{
-				token = line.substr(0,pos);
-				dataArray[i] = token;
-				line.erase(0,pos + 1);
-				++i;
-			}
-			int dataZero = atoi(dataArray[0].c_str());
-			char dataFive = line[0];
-			readUser.push_back(user(dataZero, dataArray[1], dataArray[2], dataArray[3], dataFive));
+			outfile << it->get_ID() << '/';
+			outfile << it->get_password() << '/';
+			outfile << it->get_firstName() << '/';
+			outfile << it->get_lastName() << '/';
+			outfile << it->get_classList() << '/';
+			outfile << it->get_group() << endl;
 		}
-		infile.close();
+		cout << "users.txt saved" << endl;
 	}
 	else
 	{
-		cout << "File fialed to open" << endl;
-		readUser.push_back(user(1, "password", "admin", "", 'A'));
+		cout << "users.txt failed to save" << endl;
 	}
-	return readVacancy;
 }
+
+void saveVacs(list<vacancy> saveVac)
+{
+	ofstream outfile;
+	outfile.open("vacancies.txt");
+	if (outfile.is_open())
+	{
+		for (list<vacancy>::iterator it = saveVac.begin(); it != saveVac.end(); ++it)
+		{
+			outfile << it->get_position() << '/';
+			outfile << it->get_description() << endl;
+		}
+		cout << "vacancies.txt saved" << endl;
+	}
+	else
+	{
+		cout << "vacancies.txt failed to save" << endl;
+	}
+}
+
