fb226aa8bde8ada0d9fa1d2901ae353337620c9c
andipyle14 <andipyle14@gmail.com>
2017-04-24 12:49:21 -0500

    Update AndreaFunctions.cpp

--- AndreaFunctions.cpp
+++ AndreaFunctions.cpp
@@ -13,103 +13,55 @@ void StoreTimetable()
 	int testtime; //military time, no colon
 	string class_name;
 	string description;
-	int user_id;
 
 	ofstream outfile;
-	outfile.open("TimeTable.txt");
+	outfile.open ("TimeTable.txt", ofstream::app);
 
 	if (!outfile.is_open())
 	{
 		cout << "Error. Could not open file." << endl;
 	}
-	cout << "Enter your user id: ";
-	cin >> user_id;
-	cout << endl;
-
 	cout << "Enter the month: ";
 	cin >> month;
-	cout << endl;
 	cout << "Enter the day: ";
 	cin >> day;
-	cout << endl;
 	cout << "Enter the time in military time (exclude the colon): ";
 	cin >> testtime;
-	cout << endl;
 	cout << "Enter the name of the class: ";
 	cin >> class_name;
-	cout << endl;
-	cout << "Enter the description of the test (ex. Exam 1): ";
+	cout << "Enter the description of the test, no spaces (ex. Exam1): ";
 	cin >> description;
-	cout << endl;
-
-	outfile << month << setw(4) << day << setw(4) << testtime << setw(4) << class_name << setw(4) << description << endl;
+	
+	outfile << "Month" << " " << setw(12) << "Day" << " " << setw(12) << "Test Time" << " " << setw(12) << "Class Name" << " " << setw(12) << "Description" << endl;
+	outfile << month << setw(12) << day << setw(12) << testtime << setw(12) << class_name << setw(12) << description << endl;
+	outfile << endl;
 	outfile.close();
 	cout << "Exam submitted!" << endl;
 };
-
+/* Have not gotten this function to work yet, Need to fix the infile
 void ViewTimetable()
 {
-	string month;
-	int day;
-	int testtime; //military time, no colon
-	string class_name;
-	string description;
+string month;
+int day;
+int testtime; //military time, no colon
+string class_name;
+string description;
 
-	ifstream infile;
-	infile.open("TimeTable.txt");
-	if (!infile.is_open())
-	{
-		cout << "Error. Could not open file." << endl;
-	}
-
-	cout << "Schedule" << endl;
-	cout << "Month: " << setw(4) << "Day: " << setw(4) << "Time: " << setw(4) << "Class Name: " << setw(4) << "Description: " << endl;
-
-	while (!infile.eof())
-	{
-		infile >> month >> day >> testtime >> class_name >> description >> endl; //the infile is not working. Not sure why.
-		cout << month << setw(4) << day << setw(4) << testtime << setw(4) << class_name << setw(4) << description << setw(4) << endl;
-	}
-	infile.close();
-};
-
-void main()
+ifstream infile;
+infile.open("TimeTable.txt");
+if (!infile.is_open())
 {
-	int num = 1;
-	while (num == 1)
-	{
-		int n;
-		cout << "Please enter an option:" << endl;
-		cout << "1: Store Timetable" << endl;
-		cout << "2: View Timetable" << endl;
-		cout << "3: Exit Program" << endl;
-		cin >> n;
-		while (n != NULL)
-		{
-			if (n == 1)
-			{
-				StoreTimetable();
-				break;
-			}
-			else if (n == 2)
-			{
-				ViewTimetable();
-				break;
-			}
-			else if (n == 3)
-			{
-				num = 0;
-				break;
-			}
-			else
-			{
-				cout << "You entered an invalid option. Please try again." << endl;
-			}
-		}
-	}
-
-	cout << "Thank you for using the Timetable program. Goodbye!" << endl;
-	cout << endl;
-
-	system("pause");
+cout << "Error. Could not open file." << endl;
+return;
 }
+while (getline(infile, line))
+{
+while (position = line.find("\n")) != string::npos)
+{
+
+}
+}
+infile.close();
+
+};
+*/
