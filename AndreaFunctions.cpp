60baf19f410bb3ecc6158b8a716db75299756c2c
andipyle14 <andipyle14@gmail.com>
2017-04-26 11:38:15 -0500

    Update AndreaFunctions.cpp

--- AndreaFunctions.cpp
+++ AndreaFunctions.cpp
@@ -26,19 +26,54 @@ void StoreTimetable()
 	}
 	cout << "Enter the month: ";
 	cin >> month;
+	while (outfile.is_open())
+	{
+		if (month == "January" || month == "February" || month == "March" || month == "April" || month == "May" || month == "June" || month == "July" || month == "August" || month == "September" || month == "October" || month == "November" || month == "December")
+		{
+			break;
+		}
+		else
+		{
+			cout << "You entered an invalid month. Please try again." << endl;
+			cin >> month;
+		}
+	}
 	cout << "Enter the day: ";
 	cin >> day;
+	while (day != NULL)
+	{
+		if (day > 0 || day <= 31)
+		{
+			break;
+		}
+		else
+		{
+			cout << "You entered an invalid day. Please try again." << endl;
+			cin >> day;
+		}
+	}
 	cout << "Enter the time in military time (exclude the colon): ";
 	cin >> testtime;
+	while (testtime != NULL)
+	{
+		if (testtime >= 0000 || testtime <= 2359)
+		{
+			break;
+		}
+		else
+		{
+			cout << "You entered an invalid test time. Please try again." << endl;
+			cin >> testtime;
+		}
+	}
 	cout << "Enter the name of the class: ";
 	cin >> class_name;
 	cout << "Enter short description, no spaces (ex. Exam1): ";
 	cin >> description;
 	
-	outfile << month << "/" << day << "/Time: " << testtime << "/" << class_name << "/" << description << endl;
-	outfile << endl;
+	outfile << month << "/" << day << "/" << testtime << "/" << class_name << "/" << description << endl;
 	outfile.close();
-	cout << "Exam submitted!" << endl;
+	cout << "Success!" << endl;
 };
 
 void ViewTimetable()
@@ -71,7 +106,5 @@ void ViewTimetable()
 		char dataSix = line[0];
 		cout << dataArray[0] << "/" << dataOne << "/" << dataTwo << "/" << dataArray[3] << "/" << dataArray[4] << "/" << dataArray[5] << "/" << dataSix << endl;
 	}
-
-
 	infile.close();
 };
