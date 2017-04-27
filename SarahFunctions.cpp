012009b76b47320b06241515e49d91001b6a6013
shandrock <s.handrock@siu.edu>
2017-04-26 19:16:29 -0500

    Update SarahFunctions.cpp

--- SarahFunctions.cpp
+++ SarahFunctions.cpp
@@ -1,29 +1,32 @@
 #include "SarahFunctions.h"
-#include "structures.h"
 #include <list>
 #include <iostream>
 #include <iterator>
 #include <string>
 
-int logIn(list<user> userlist)
+user logIn(list<user> userlist, int *prog)
 {
-  int queryID;
-  string queryPass;
-  cout<<"Please enter your ID: ";
-  cin >> queryID;
-  cout<<"Please enter your password:  ";
-  cin >> queryPass;
-  
-  for (list<user>::iterator it = userlist.begin(); it != userlist.end(); ++it)
+	int queryID;
+	string queryPass;
+	string pass;
+	int ID;
+	cout << "Please enter your ID: ";
+	cin >> queryID;
+	cout << "Please enter your password:  ";
+	cin >> queryPass;
+
+	for (list<user>::iterator it = userlist.begin(); it != userlist.end(); ++it)
 	{
-		if(it->ID == queryID && it->password == queryPass)
+		ID = it->get_ID();
+		pass = it->get_password();
+		if (ID == queryID && pass == queryPass)
 		{
 			cout << "Valid login" << endl;
-			return 1;
-			break;
+			*prog = 1;
+			return *it;
 		}
-		  
-   }
-cout<<"Invalid Username/Password"<<endl;
-return 0;
+
+	}
+	cout << "Invalid Username/Password" << endl;
+	*prog = 0;
 }
