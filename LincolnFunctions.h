3570ec85be300b81813f593335280844c7782aa3
LincolnK <lincoln.kinley@siu.edu>
2017-04-26 23:09:56 -0500

    Update LincolnFunctions.h

--- LincolnFunctions.h
+++ LincolnFunctions.h
@@ -6,13 +6,16 @@
 
 
 list<user> readUsers();
-//list<course> readCourses();
+list<course> readCourses();
 list<vacancy> readVacs();
 void saveUsers(list<user> saveUser);
+void saveCourse(list<course> saveCourse);
+void joinCourse(list<course>& courseList, string title, int ID);
 void newVacancy(list<vacancy>& vacList, string positionIn, string discriptionIn);
 void newUser(list<user>& userList, int ID, string password, string firstName, string lastName, char group);
 void removeVacancy(list<vacancy>& vacList, string del);
 void removeUser(list<user>& userList, int ID);
 void saveVacs(list<vacancy> saveVac);
+void take_attendence(list<course> courses, user current);
 
 #endif // LINCOLNFUNCTIONS_H_
