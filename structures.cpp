a152f5668a592457875f9ec81a02379652ceeca5
LincolnK <lincoln.kinley@siu.edu>
2017-05-08 13:16:14 -0500

    Add files via upload

---
+++ structures.cpp
@@ -0,0 +1,135 @@
+#include <string>
+#include <iostream>
+#include "structures.h"
+
+using namespace std;
+
+
+//------------------------------------------------------
+//			User member functions
+//------------------------------------------------------
+
+
+user::user(int a, string b, string c, string d, string e, char f)
+{
+	setUser(a, b, c, d, e, f);
+}
+
+user::user()
+{
+	ID = 0;
+}
+
+void user::setUser(int a, string b, string c, string d, string e, char f)
+{
+	ID = a;
+	password = b;
+	firstName = c;
+	lastName = d;
+	classList = e;
+	group = f;
+}
+
+int user::get_ID()
+{
+	return ID;
+}
+
+string user::get_firstName()
+{
+	return firstName;
+}
+
+string user::get_lastName()
+{
+	return lastName;
+}
+
+string user::get_password()
+{
+	return password;
+}
+
+char user::get_group()
+{
+	return group;
+}
+
+string user::get_classList()
+{
+	return classList;
+}
+
+void user::set_password(string newPass)
+{
+	password = newPass;
+}
+
+void user::set_classList(string a)
+{
+	classList = a;
+}
+
+
+//------------------------------------------------------
+//			Course member functions
+//------------------------------------------------------
+
+
+course::course(string a, string b)
+{
+	setCourse(a, b);
+}
+
+course::course(string a)
+{
+	title = a;
+}
+
+void course::setCourse(string a, string b)
+{
+	title = a;
+	attendance = b;
+}
+
+string course::get_title()
+{
+	return title;
+}
+
+string course::get_attendance()
+{
+	return attendance;
+}
+
+void course::setAttendance(string a)
+{
+	attendance = a;
+}
+
+
+//------------------------------------------------------
+//			Vacany member functions
+//------------------------------------------------------
+
+
+vacancy::vacancy(string a, string b)
+{
+	setVacancy(a, b);
+}
+
+void vacancy::setVacancy(string a, string b)
+{
+	position = a;
+	discription = b;
+}
+
+string vacancy::get_position()
+{
+	return position;
+}
+
+string vacancy::get_description()
+{
+	return discription;
+}
--- structures.h
+++ structures.h
@@ -7,116 +7,46 @@
 using namespace std;
 
 class user {
-  int ID;
-  string password;
-  string firstName;
-  string lastName;
-  string classList; //Will store the class list for the student
-  char group; // S (student), F (faculty), A (admin)
+	int ID;
+	string password;
+	string firstName;
+	string lastName;
+	string classList; //Will store the class list for the student
+	char group; // S (student), F (faculty), A (admin)
 public:
-  user::user(int a, string b, string c, string d, string e, char f)
-	{
-		setUser(a, b, c, d, e, f);
-	}
-  user::user()
-  {
-	  ID = 0;
-  }
-  void setUser(int a, string b, string c, string d, string e, char f)
-  {
-    ID = a;
-    password = b;
-    firstName = c;
-    lastName = d;
-    classList = e;
-	group = f;
-  }
-  int get_ID()
-  {
-    return ID;
-  }
-  string get_firstName()
-  {
-    return firstName;
-  }
-  string get_lastName()
-  {
-    return lastName;
-  }
-  string get_password()
-	{
-	return password;
-	}
-  char get_group()
-  {
-    return group;
-  }
-  string get_classList()
-  {
-	  return classList;
-  }
-  void set_password(string newPass)
-  {
-	  password = newPass;
-  }
-  void set_classList(string a)
-  {
-	  classList = a;
-  }
+	user::user(int a, string b, string c, string d, string e, char f);
+	user::user();
+	void setUser(int a, string b, string c, string d, string e, char f);
+	int get_ID();
+	string get_firstName();
+	string get_lastName();
+	string get_password();
+	char get_group();
+	string get_classList();
+	void set_password(string newPass);
+	void set_classList(string a);
 };
 
 class course {
 	string title;
 	string attendance; // ID number goes in the first column, then it will say 1 for present or 2 for not present in the 2nd column
 public:
-	course::course(string a, string b)
-	{
-		setCourse(a, b);
-	}
-	course::course(string a)
-	{
-		title = a;
-	}
-	void setCourse(string a, string b)
-	{
-		title = a;
-		attendance = b;
-	}
-	string get_title()
-	{
-		return title;
-	}
-	string get_attendance()
-	{
-		return attendance;
-	}
-	void setAttendance(string a)
-	{
-		attendance = a;
-	}
+	course::course(string a, string b);
+	course::course(string a);
+	void setCourse(string a, string b);
+	string get_title();
+	string get_attendance();
+	void setAttendance(string a);
 };
 
 class vacancy {
 	string position;
 	string discription;
 public:
-	vacancy::vacancy(string a, string b)
-	{
-		setVacancy(a, b);
-	}
-  void setVacancy(string a, string b)
-	{
-		position = a;
-		discription = b;
-	}
-	string get_position()
-	{
-		return position;
-	}
-	string get_description()
-	{
-		return discription;
-	}
+	vacancy::vacancy(string a, string b);
+	void setVacancy(string a, string b);
+	string get_position();
+	string get_description();
 };
 
 
